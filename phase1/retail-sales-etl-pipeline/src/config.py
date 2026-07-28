b7f39e93aa20a71b23859bbf962bcfa693055c23
martinpapaleo <martinpapaleo1@gmail.com>
2026-07-27 22:03:06 -0300

    changes

--- phase1/retail-sales-etl-pipeline/src/config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = ROOT_DIR / 'data'
-RAW_DIR = DATA_DIR / 'raw'
-PROCESSED_DIR = DATA_DIR / 'processed'
-RAW_FILE_PATH = RAW_DIR / 'supermarket_sales.csv'
-OUTPUT_FILE_PATH = PROCESSED_DIR / 'sales_clean.parquet'
